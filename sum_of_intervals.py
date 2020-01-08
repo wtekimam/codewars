edc15a0c184f5bab75cdc9922bf1edc2be1bcf2e
William Tekimam <william@treeclouds.com>
2020-01-08 10:58:26 +0700

    Create interval using set

---
+++ sum_of_intervals.py
@@ -0,0 +1,22 @@
+import unittest
+
+
+def sum_of_intervals(intervals):
+    output = set()
+    for interval in intervals:
+        for i in range(interval[0], interval[1]):
+            output.add(i)
+
+    return len(output)
+
+
+class TestStuff(unittest.TestCase):
+    def test_stuff(self):
+        self.assertEqual(sum_of_intervals([(1, 5)]), 4)
+        self.assertEqual(sum_of_intervals([(1, 5), (6, 10)]), 8)
+        self.assertEqual(sum_of_intervals([(1, 5), (1, 5)]), 4)
+        self.assertEqual(sum_of_intervals([(1, 4), (7, 10), (3, 5)]), 7)
+
+
+if __name__ == '__main__':
+    unittest.main()
